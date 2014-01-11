c9c9cda898485ba89e0759689949b2bd1ad87904
owlbytes <neshah07@gmail.com>
2014-01-11 17:39:39 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmyTina::Application.config.secret_token = '9c5a6ace82c94eb829e300da1c6fde6e0277bdbf88e68bb9c397a20461a545d78b13e9e2392765567391da25835c98f69ff6e825ee99145b2b3fc5de8320ff49'
