ee3c3c9f1c28254132e78b8ac80e51900e736025
owlbytes <neshah07@gmail.com>
2014-01-12 22:15:49 +0000

    Tina seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,56 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
+#Quotes From Tina
+
 post1 = Post.create!(ta: "Tina", text: "Tina Tina Tina")
 
+post2 = Post.create!(ta: "Tina", text: "Some people say, 'Never let them see you cry.' I say, if you’re so mad you could just cry, then cry. It terrifies everyone.")
+
+post3 = Post.create!(ta: "Tina", text: "Don't be too precious or attached to anything you write. Let things be malleable. For sketch writers, remember they're called sketches for a reason. They're not called oil paintings. Some of them are going to stink. You have to let them stink.")
+
+post4 = Post.create!(ta: "Tina", text: "You can’t be that kid standing at the top of the waterslide, overthinking it. You have to go down the chute.")
+
+post5 = Post.create!(ta: "Tina", text: "If you retain nothing else, always remember the most important rule of beauty, which is: who cares?")
+
+post6 = Post.create!(ta: "Tina", text: "Whatever the problem, be part of the solution. Don’t just sit around raising questions and pointing out obstacles.")
+
+post7 = Post.create!(ta: "Tina", text: "I know for sure that you can tell how smart people are by what they laugh at.")
+
+post8 = Post.create!(ta: "Tina", text: "Do your thing and don't care if they like it.")
+
+post9 = Post.create!(ta: "Tina", text: "Confidence is 10 percent hard work and 90 percent delusion.")
+
+post10 = Post.create!(ta: "Tina", text: "Wear a bra, don't smoke.")
+
+post11 = Post.create!(ta: "Tina", text: "It is an impressively arrogant move to conclude that just because you don’t like something, it is empirically not good.")
+
+post12 = Post.create!(ta: "Tina", text: "If you retain nothing else, always remember the most important rule of beauty, which is: who cares?")
+
+post13 = Post.create!(ta: "Tina", text: "So, my unsolicited advice to women in the workplace is this. When faced with sexism, or ageism, or lookism, or even really aggressive Buddhism, ask yourself the following question: “Is this person in between me and what I want to do?” If the answer is no, ignore it and move on. Your energy is better used doing your work and outpacing people that way. Then, when you’re in charge, don’t hire the people who were jerky to you.")
+
+post14 = Post.create!(ta: "Tina", text: "Whatever the problem, be part of the solution. Don’t just sit around raising questions and pointing out obstacles.")
+
+post15 = Post.create!(ta: "Tina", text: "THERE ARE NO MISTAKES, only opportunities.")
+
+post16 = Post.create!(ta: "Tina", text: "This is what I tell young women who ask me for career advice. People are going to try to trick you. To make you feel that you are in competition with one another. 'You're up for a promotion. If they go for a woman, it'll be between you and Barbara.' Don't be fooled. You're not in competition with other women. You're in competition with everyone.")
+
+post17 = Post.create!(ta: "Tina", text: "MAKE STATEMENTS also applies to us women: Speak in statements instead of apologetic questions. No one wants to go to a doctor who says, “I’m going to be your surgeon? I’m here to talk to you about your procedure? I was first in my class at Johns Hopkins, so?” Make statements, with your actions and your voice.")
+
+post18 = Post.create!(ta: "Tina", text: "When people say, 'You really, really must' do something, it means you don't really have to. No one ever says, 'You really, really must deliver the baby during labor.' When it's true, it doesn't need to be said.")
+
+post19 = Post.create!(ta: "Tina", text: "Let's admit it, yellow hair does have magic powers. You could put a blond wig on a hot-water heater and some dude would try to fuck it.")
+
+post20 = Post.create!(ta: "Tina", text: "You must not look in that mirror at your doughy legs and flat feet, for today is about dreams and illusions, and unfiltered natural daylight is the enemy of dreams.")
+
+post21 = Post.create!(ta: "Tina", text: "When choosing sexual partners, remember: Talent is not sexually transmittable.")
+
+post22 = Post.create!(ta: "Tina", text: "There is no one of-woman-born who does not like Red Lobster cheddar biscuits. Anyone who claims otherwise is a liar and a Socialist. ")
+
+post23 = Post.create!(ta: "Tina", text: "We should leave people alone about their weight. Being skinny for a while (provided you actually eat food and don't take pills or smoke to get there) is a perfectly fine pastime. Everyone should try it once, like a supershort haircut or dating a white guy.")
+ 
+
+#Quotes From Amy 
 post2 = Post.create!(ta: "Amy", text: "Amy Amy Amy")
 
  
