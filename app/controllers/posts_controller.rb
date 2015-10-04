43d1a5622c7eece09c68b4ddcea4dff823d0a21a
owlbytes <neshah07@gmail.com>
2015-10-04 14:35:50 +0100

    making it easier to add new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -80,7 +80,7 @@ class PostsController < ApplicationController
     @post.destroy
 
     respond_to do |format|
-      format.html { redirect_to posts_url }
+      format.html { redirect_to admins_url }
       format.json { head :no_content }
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-# post1 = Post.create!(ta: "Tina", text: "Tina Tina Tina")
-
-#Quotes From Tina
 
 post2 = Post.create!(ta: "Tina", text: "Some people say, 'Never let them see you cry.' I say, if you’re so mad you could just cry, then cry. It terrifies everyone.")
 
@@ -16,104 +5,11 @@ post3 = Post.create!(ta: "Tina", text: "Don't be too precious or attached to any
 
 post4 = Post.create!(ta: "Tina", text: "You can’t be that kid standing at the top of the waterslide, overthinking it. You have to go down the chute.")
 
-post5 = Post.create!(ta: "Tina", text: "If you retain nothing else, always remember the most important rule of beauty, which is: who cares?")
-
-post6 = Post.create!(ta: "Tina", text: "Whatever the problem, be part of the solution. Don’t just sit around raising questions and pointing out obstacles.")
-
-post7 = Post.create!(ta: "Tina", text: "I know for sure that you can tell how smart people are by what they laugh at.")
-
-post8 = Post.create!(ta: "Tina", text: "Do your thing and don't care if they like it.")
-
-post9 = Post.create!(ta: "Tina", text: "Confidence is 10 percent hard work and 90 percent delusion.")
-
-post10 = Post.create!(ta: "Tina", text: "Wear a bra, don't smoke.")
-
-post11 = Post.create!(ta: "Tina", text: "It is an impressively arrogant move to conclude that just because you don’t like something, it is empirically not good.")
-
-post12 = Post.create!(ta: "Tina", text: "If you retain nothing else, always remember the most important rule of beauty, which is: who cares?")
-
-post13 = Post.create!(ta: "Tina", text: "So, my unsolicited advice to women in the workplace is this. When faced with sexism, or ageism, or lookism, or even really aggressive Buddhism, ask yourself the following question: “Is this person in between me and what I want to do?” If the answer is no, ignore it and move on. Your energy is better used doing your work and outpacing people that way. Then, when you’re in charge, don’t hire the people who were jerky to you.")
-
-post14 = Post.create!(ta: "Tina", text: "Whatever the problem, be part of the solution. Don’t just sit around raising questions and pointing out obstacles.")
-
-post15 = Post.create!(ta: "Tina", text: "THERE ARE NO MISTAKES, only opportunities.")
-
-post16 = Post.create!(ta: "Tina", text: "This is what I tell young women who ask me for career advice. People are going to try to trick you. To make you feel that you are in competition with one another. 'You're up for a promotion. If they go for a woman, it'll be between you and Barbara.' Don't be fooled. You're not in competition with other women. You're in competition with everyone.")
-
-post17 = Post.create!(ta: "Tina", text: "MAKE STATEMENTS also applies to us women: Speak in statements instead of apologetic questions. No one wants to go to a doctor who says, “I’m going to be your surgeon? I’m here to talk to you about your procedure? I was first in my class at Johns Hopkins, so?” Make statements, with your actions and your voice.")
-
-post18 = Post.create!(ta: "Tina", text: "When people say, 'You really, really must' do something, it means you don't really have to. No one ever says, 'You really, really must deliver the baby during labor.' When it's true, it doesn't need to be said.")
-
-post19 = Post.create!(ta: "Tina", text: "Let's admit it, yellow hair does have magic powers. You could put a blond wig on a hot-water heater and some dude would try to fuck it.")
-
-post20 = Post.create!(ta: "Tina", text: "You must not look in that mirror at your doughy legs and flat feet, for today is about dreams and illusions, and unfiltered natural daylight is the enemy of dreams.")
-
-post21 = Post.create!(ta: "Tina", text: "When choosing sexual partners, remember: Talent is not sexually transmittable.")
-
-post22 = Post.create!(ta: "Tina", text: "There is no one of-woman-born who does not like Red Lobster cheddar biscuits. Anyone who claims otherwise is a liar and a Socialist. ")
-
-post23 = Post.create!(ta: "Tina", text: "We should leave people alone about their weight. Being skinny for a while (provided you actually eat food and don't take pills or smoke to get there) is a perfectly fine pastime. Everyone should try it once, like a supershort haircut or dating a white guy.")
- 
-
-
-
-
-
-
- 
-
-#Quotes From Amy 
-
-post24 = Post.create!(ta: "Amy", text: "The great thing about taking big chances when you're younger is you have less to lose, and you don't know as much. So you take big swings.")
-
-post25 = Post.create!(ta: "Amy", text: "I get worried for young girls sometimes. I want them to feel that they can be sassy and full and weird and geeky and smart and independent and not so withered and shriveled. ")
-
-post26 = Post.create!(ta: "Amy", text: "There's power in looking silly and not caring that you do.")
-
-post27 = Post.create!(ta: "Amy", text: "Rooting for other people's failure does get in the way of your success.")
-
-post28 = Post.create!(ta: "Amy", text: "Apologies can be simple. They don't have to be grandiose and it's never too late to apologize.")
-
-post29 = Post.create!(ta: "Amy", text: "Girls, if a boy says something that isn't funny you dont have to laugh. ")
-
-post30 = Post.create!(ta: "Amy", text: "Take your risks now, as you grow older you become more fearful and less flexible. And I mean that litterally. I hurt my knee this week ona  treadmill and it wasn't even on. ")
-
-post31 = Post.create!(ta: "Amy", text: "Anybody who doesn't make you feel good kick them to the curb. And the earlier you start in your life, the better.")
-
-post32 = Post.create!(ta: "Amy", text: "Continue to share your heart with people even if it's been broken.")
-
-post33 = Post.create!(ta: "Amy", text: "If you can speak about what you care about to a person you disagree with without denigrating or insulting htem, then you may actually be heard and you might even change their mind.")
-
-post34 = Post.create!(ta: "Amy", text: "When you feel scared, hold someone's hand and look into their eyes. And when you feel brave, do the same thing.")
-
-post35 = Post.create!(ta: "Amy", text: "Try to keep your mind open to possibilities and your mouth closed on matters that you don’t know about. Limit your ‘always’ and your ‘nevers.’")
-
-post36 = Post.create!(ta: "Amy", text: "Try putting your iPhones down every once in a while and look at people's faces.")
-
-post37 = Post.create!(ta: "Amy", text: "Vulnerable people are powerful people. Opening your heart and sharing it means you're going to get so much love in your life.")
-
-post38 = Post.create!(ta: "Amy", text: "If you can sleep, breathe, work hard, wish for the best and do everything you can to prepare, I bet you'll do well -- as well as you can. And that's all anybody can ask for.")
-
-post39 = Post.create!(ta: "Amy", text: "Guys like girls who don't wear a lot of makeup. You have your whole life to have to wear a lot of makeup.")
-
-post40 = Post.create!(ta: "Amy", text: "You have to remember that goodbyes are temporary because no one ever really leaves and nothing lasts forever. People are always with us, because they are in our hearts and in our memory. The only thing we can depend on is change... Life is just a series of moments -- a string of pearls that make up the necklace of your life and so every once in a while to complete the circle, you need to end a chapter.")
-
-post41 = Post.create!(ta: "Amy", text: "We don't want to be afraid to make a choice because we're afraid to make a mistake because most decisions aren't final. Feelings change all the time. You can always change your mind and taking risks and making choices is what makes life so exciting because we never know what's going to happen. Every day something new comes our way.")
-
-post42 = Post.create!(ta: "Amy", text: "Great people do things before they’re ready. They do things before they know they can do it. Doing what you're afraid of, getting out of your comfort zone, taking risks like that -- that is what life is. You might be really good. You might find out something about yourself that's really special and if you're not good, who cares? You tried something. Now you know something about yourself.")
-
-post43 = Post.create!(ta: "Amy", text: "Sometimes jealousy masks itself as anger or resentment, and it’s good to just straight-up admit sometimes, ‘Man, I’m jealous of that.’")
-
-post44 = Post.create!(ta: "Amy", text: "It’s never overreacting to ask for what you want and need.")
-
 post45 = Post.create!(ta: "Amy", text: "What’s cool about volunteering is, once you become passionate about things, passion is what teaches us who we are and what we want our world to look like and the kind of people we want to have in our lives. So start figuring out what excites you, or what angers you, and fight for or against those things.")
 
 post46 = Post.create!(ta: "Amy", text: "Fathers do such a good job when they make their daughters feel like they can do anything. That’s what fathers are supposed to do: make their daughters feel loved and safe and strong.")
 
 post47 = Post.create!(ta: "Amy", text: "Try to choose your words a little bit more carefully. I know it’s hard, sometimes we lose control. But if you can try to think about a way to be kind with your words, it may make a difference.")
-
-# post2 = Post.create!(ta: "Amy", text: "Amy Amy Amy")
-
 
  
 
